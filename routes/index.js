744e6654f2f70163516ea666d43b9f7c241d353a
Alexpra2 <alexpra2@gmail.com>
2025-02-06 12:37:10 +0100

    correccion fallo

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const usersRoutes = require('./users');
+const usersRoutes = require('./user');
 
 router.use('/', usersRoutes);
 
